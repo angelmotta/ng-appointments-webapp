78ea0e4f622033a2d914f58ad18a9c82581f2360
Angel Motta <angel.motta@utec.edu.pe>
2024-10-28 12:46:08 -0500

    feat: get appointments from Rest API

--- src/app/appointments/service/appointment.service.ts
+++ src/app/appointments/service/appointment.service.ts
@@ -9,10 +9,12 @@ export class AppointmentService {
   constructor(private http: HttpClient) {}
 
   getAppointment(): Observable<any> {
+    const API_APPOINTMENTS_URL = 'http://localhost:8080/api/appointments';
+
     console.log(
-      'getAppointment method called - but HTTP request not yet made!'
+      'getAppointment service method called - but HTTP request not yet made!'
     );
-    return this.http.get('http://localhost:3000/citas').pipe(
+    return this.http.get(API_APPOINTMENTS_URL).pipe(
       tap({
         next: () =>
           console.log('HTTP Request executed - this means someone subscribed!'),
