7d117b34b76272385d1d114037773b443e3536d7
Angel Motta <angel.motta@utec.edu.pe>
2024-10-29 15:56:22 -0500

    feat: setup reactive form to request new appointment

--- src/app/appointments/request-appointment/request-appointment.component.ts
+++ src/app/appointments/request-appointment/request-appointment.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-request-appointment',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './request-appointment.component.html',
-  styleUrl: './request-appointment.component.css'
+  styleUrl: './request-appointment.component.css',
 })
 export class RequestAppointmentComponent {
+  requestAppointmentForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    dni: new FormControl(''),
+    specialtyId: new FormControl(''),
+  });
 
+  handleSubmit() {
+    console.log(this.requestAppointmentForm.value);
+  }
 }
