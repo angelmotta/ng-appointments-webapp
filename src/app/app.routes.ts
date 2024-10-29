373ba9e2ca5ac97da4267da50c7ed1101ac81366
Angel Motta <angel.motta@utec.edu.pe>
2024-10-28 19:31:27 -0500

    feat: adding router navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppointmentsListComponent } from './appointments/appointments-list/appointments-list.component';
+import { RequestAppointmentComponent } from './appointments/request-appointment/request-appointment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Bienestar citas',
+    component: AppointmentsListComponent,
+  },
+  {
+    path: 'cita',
+    title: 'Crear cita',
+    component: RequestAppointmentComponent,
+  },
+];
---
+++ src/app/appointments/request-appointment/request-appointment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-appointment.component.html',
+  styleUrl: './request-appointment.component.css'
+})
+export class RequestAppointmentComponent {
+
+}
