ae373c6d70d12f30efcf7b83fc48e86b634d8e0f
Angel Motta <angel.motta@utec.edu.pe>
2024-11-10 13:39:59 -0500

    feat: include nav bar from Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppointmentsListComponent } from './appointments/appointments-list/appointments-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, AppointmentsListComponent],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    AppointmentsListComponent,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'appointments-webapp';
+
+  onCreateAppointment() {
+    console.log('Create appointment clicked');
+    // routerLink="/cita"
+  }
 }
